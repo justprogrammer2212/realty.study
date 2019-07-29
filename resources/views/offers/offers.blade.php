255a7d83f58fde5987674a66c1975fbcbf665bd1
justprogrammer2212 <justprogrammer2212@gmail.com>
2019-07-29 09:07:37 +0300

    fix

--- resources/views/offers/offers.blade.php
+++ resources/views/offers/offers.blade.php
@@ -15,87 +15,41 @@
             <div class="jumbotron p-4 p-md-5 text-dark rounded bg-light">
                 <div class="col-md-6 px-0">
                     <h1 class="display-4 font-italic">Продаж будинків</h1>
-                    <p class="lead my-3">Тут розміщені будинки які на нашу думку найкраще підходять для Вашого проживання</p>
+                    <p class="lead my-3">Тут розміщені будинки які на нашу думку найкраще підходять для Вашого
+                        проживання</p>
                     <p class="lead mb-0"><a href="#" class="text-dark font-weight-bold">Читати далі...</a></p>
                 </div>
             </div>
 
             <div class="row mb-2">
-                <div class="col-md-6">
-                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-success">$120 000</strong>
-                            <h3 class="mb-0">Продаж/Особняки</h3>
-                            <div class="mb-1 text-muted">Сьогодні о 12:00</div>
-                            <p class="card-text mb-auto">Продам особняк в хорошому стані</p>
+                @foreach($offers as $offer)
+                    <div class="col-md-6">
+                        <div
+                            class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                                <strong
+                                    class="d-inline-block mb-2 text-success">{{$offer->price}} {{$offer->currency}}</strong>
+                                <h3 class="mb-0">{{$offer->name}}</h3>
+                                <div class="mb-1 text-muted">{{$offer->created_at}}</div>
+                                <p class="card-text mb-auto">{{strlen($offer->description) >= 100 ? substr($offer->description,0,200). '...' : $offer->description}}</p>
 
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                            <img src="/images/home.jpg" class="img-thumbnail alt="...">
-                            <p class="text-center mt-3 mb-2">
-                                <button class="btn btn-outline-primary">Continue reading</button>
-                            </p>
+                            </div>
+                            <div class="col-auto d-none d-lg-block">
+                                <img src="/images/home.jpg" class="img-thumbnail alt=" ...">
+                                <p class="text-center mt-3 mb-2">
+                                    <a class="btn btn-outline-primary" href="{{route('show', $offer)}}">Читати
+                                        оголошення</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-success">$120 000</strong>
-                            <h3 class="mb-0">Продаж/Особняки</h3>
-                            <div class="mb-1 text-muted">Сьогодні о 12:00</div>
-                            <p class="card-text mb-auto">Продам особняк в хорошому стані</p>
-
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                            <img src="/images/home.jpg" class="img-thumbnail alt="...">
-                            <p class="text-center mt-3 mb-2">
-                                <button class="btn btn-outline-primary">Continue reading</button>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mb-2">
-                <div class="col-md-6">
-                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-success">$120 000</strong>
-                            <h3 class="mb-0">Продаж/Особняки</h3>
-                            <div class="mb-1 text-muted">Сьогодні о 12:00</div>
-                            <p class="card-text mb-auto">Продам особняк в хорошому стані</p>
-
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                            <img src="/images/home.jpg" class="img-thumbnail alt="...">
-                            <p class="text-center mt-3 mb-2">
-                                <button class="btn btn-outline-primary">Continue reading</button>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-success">$120 000</strong>
-                            <h3 class="mb-0">Продаж/Особняки</h3>
-                            <div class="mb-1 text-muted">Сьогодні о 12:00</div>
-                            <p class="card-text mb-auto">Продам особняк в хорошому стані</p>
-
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                            <img src="/images/home.jpg" class="img-thumbnail alt="...">
-                            <p class="text-center mt-3 mb-2">
-                                <button class="btn btn-outline-primary">Continue reading</button>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
 
         </div>
-
+        <nav class="pagination justify-content-center">
+            {!!$offers->links();!!}
+        </nav>
     </main>
 
 @stop
